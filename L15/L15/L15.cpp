78b38a5c05be8eb859b90e3ef7e6e12dfbb70d2e
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2021-10-30 10:25:15 +0300

    Add seconds in minute

--- L15/L15/L15.cpp
+++ L15/L15/L15.cpp
@@ -6,3 +6,13 @@ int main()
     std::cout << "Hello World!\n";
 }
 
+bool isLeapYear(int year)
+{
+    return (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0);
+}
+
+int SecondsInMinutes()
+{
+    return 60;
+}
+
