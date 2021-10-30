eea617cae49a3a52a7eb6013c227358801c37b98
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2021-10-30 10:38:47 +0300

    Add days in year

--- L15/L15/L15.cpp
+++ L15/L15/L15.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <iostream>
+#include <cassert>
 
 int main()
 {
@@ -11,4 +12,18 @@ bool isLeapYear(int year)
     return (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0);
 }
 
+int LaterInDay(int first, int second)
+{
+    assert(first < 366 && first >0 && second < 366 && second>0);
+    return std::max(first, second);
+}
+
+int DaysInYear(int year)
+{
+    if (isLeapYear(year)) return 366;
+    else
+    {
+        return 365;
+    }
+}
 
